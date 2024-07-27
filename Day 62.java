2bcd928fcb8e949dcefa7b051d4941912ca5b74a
Astha Rajput <98215717+7-astha-rajput@users.noreply.github.com>
2024-07-27 22:37:59 +0530

    Create Day 62.java

---
+++ Day 62.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // Create a dummy node to act as the head of the merged list
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        
+        // Iterate through both lists while both have nodes
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+        
+        // At this point, one of the lists might still have nodes left
+        // Append the remaining nodes to the merged list
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+        
+        // The merged list is next to the dummy node
+        return dummy.next;
+    }
+}
